9bb3fb6e46f3511bb9fef97a70c6d1f4e5a8dc99
DarienIgnacio <dari.urrutia@duocuc.cl>
2025-11-26 12:33:50 -0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/LevelUpGamerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpGamerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpGamerAppApplication.class, args);
+	}
+
+}
